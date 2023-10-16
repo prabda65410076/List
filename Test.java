1e1226d1d1fccd0f5d2529e3968dea03496cf57b
prabda65410076 <146711694+prabda65410076@users.noreply.github.com>
2023-10-16 14:46:20 +0700

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,25 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		ArrayList<Character> ar = new ArrayList<>();
+
+		ar.add(0, 'A');
+		ar.add(0, 'B');
+		ar.get(1);
+		System.out.println("Get :"+ar.get(1));
+		System.out.println("Error");
+		//ar.set(2, 'C');
+		ar.add(2, 'C');
+		System.out.println("Error");
+		//ar.add(4, 'd');
+		System.out.println("Remove :"+ar.remove(1));
+		ar.add(1, 'D');
+		ar.add(1, 'E');
+		//ar.get(4);
+		System.out.println("Error");
+		ar.add(4, 'F');
+		System.out.println("Set :"+ar.set(2,'G'));
+		System.out.println("Get :"+ar.get(2));
+	}
+}
